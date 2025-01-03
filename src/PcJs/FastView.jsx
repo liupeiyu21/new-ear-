5f86752fbb3d571e3e14f5a2c2f2768f8472973f
Liu Peiyu <24aw0142@jec.ac.jp>
2025-01-03 15:55:01 +0900

    fastviewの部分を終わった

---
+++ src/PcJs/FastView.jsx
@@ -0,0 +1,81 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import Swiper1 from "/pc-top-swiper1.png";
+import Swiper2 from "/pc-top-swiper2.png";
+import Swiper3 from "/pc-top-swiper3.png";
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import './styles.css';
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
+
+export default function FastView() {
+  return (
+    <>
+    <div className='fastview-swiper'>
+      <Swiper
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        breakpoints={{
+            640:{
+                slidesPerView:1,
+                spaceBetween:10,
+            },
+            768: {
+                slidesPerView:2,
+                spaceBetween:0,
+            },
+            1024:{
+                slidesPerView:4,
+                spaceBetween:15,
+            }
+        }}
+        loop = {{loop: true}}
+        scrollbar={{ draggable: true }}
+        pagination={{ clickable: true }}
+        cssMode={true}
+        navigation={true}
+        // pagination={true}
+        mousewheel={true}
+        keyboard={true}
+
+
+        className="mySwiper"
+      >
+        <SwiperSlide>
+            <img src={Swiper1} alt="バナーの画像" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={Swiper2} alt="バナーの画像" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={Swiper3} alt="バナーの画像" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={Swiper1} alt="バナーの画像" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={Swiper2} alt="バナーの画像" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={Swiper3} alt="バナーの画像" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={Swiper1} alt="バナーの画像" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={Swiper2} alt="バナーの画像" />
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={Swiper3} alt="バナーの画像" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+    </>
+  );
+}
